12705688f406bbd6171cfebc2018106d5aa64db0
Gustavo <gustavomarquesmm@gmail.com>
2021-10-14 18:42:23 -0300

    Criação de um sistema de Fotos com React e Firebase

---
+++ src/services/photos.ts
@@ -0,0 +1,24 @@
+import { Photo } from '../types/Photo';
+import { storage } from '../libs/firebase'
+import { ref, listAll, getDownloadURL } from 'firebase/storage'
+
+
+export const getAll = async () => {
+    let list: Photo[] = [];
+
+    const imagesFolder = ref(storage,'images'); /* Cria referencia da pasta */
+    const photoList = await listAll(imagesFolder); /* Lista todos os arquivos da pasta */
+
+    for(let i in photoList.items) {
+        let photoUrl = await getDownloadURL(photoList.items[i]); /* Gera todos as URLS de download para o Firebase*/
+
+        /* Monta o Array */
+        list.push({
+            name: photoList.items[i].name,
+            url: photoUrl
+        })
+    }
+
+    /* Retorna o Array */
+    return list;
+}
