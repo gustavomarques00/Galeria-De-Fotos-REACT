8cadca42272500b88f4a91858287adee1fb1ef94
Gustavo <gustavomarquesmm@gmail.com>
2021-10-14 21:23:29 -0300

    Sistema de Fotos Completo

--- src/components/PhotoItem/index.tsx
+++ src/components/PhotoItem/index.tsx
@@ -1,9 +1,15 @@
 import * as C from './styles';
 
-export const PhotoItem = () => {
+type Props = {
+    url: string;
+    name: string;
+}
+
+export const PhotoItem = ({ url, name }: Props) => {
     return(
         <C.Container>
-            
+            <img src={url} alt={name} />
+            {name}
         </C.Container>
     )
 }
